03f2502ca40ca84d5fecd66825677087c44d07dc
SachinDhakarey <sachindhakarey2002@gmail.com>
2023-06-23 17:59:02 +0530

    update the header,about, homepage etc

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 //alert("Hello")
 
 setTimeout(function(){
-    alert("Iagree to the cookies")
+    alert("Accept cookies from this site")
 },3000)
+
+$("#search-icon").click(function() {
+    $(".nav").toggleClass("search");
+    $(".nav").toggleClass("no-search");
+    $(".search-input").toggleClass("search-active");
+  });
+  
+  $('.menu-toggle').click(function(){
+     $(".nav").toggleClass("mobile-nav");
+     $(this).toggleClass("is-active");
+  });
